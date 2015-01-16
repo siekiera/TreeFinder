15dbeb15956fca3303bc06d4f6f649c81210b207
siekiera <mistrzsiekiera@gmail.com>
2015-01-16 21:42:47 +0100

    Zwielokrotnienie testów

--- src/test/java/pl/edu/pw/elka/treefinder/logic/algorithm/ComplexityTest.java
+++ src/test/java/pl/edu/pw/elka/treefinder/logic/algorithm/ComplexityTest.java
@@ -21,7 +21,8 @@ public class ComplexityTest {
 
     private final MSTAlgorithm[] algorithms = {new PrimAlgorithm(), new KruskalAlgorithm()};
     private static final float[] DENSITIES = {0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f};
-    private static final int[] VERTEX_COUNTS = {50, 100, 150, 200, 250, 300};
+    private static final int[] VERTEX_COUNTS = {50, 100, 150, 200, 250, 300, 350, 400};
+    private static final int NUMBER_OF_TESTS = 5;
     private final GraphGenerator generator = new GraphGeneratorImpl();
 
 
@@ -33,16 +34,23 @@ public class ComplexityTest {
         }
         for (int i = 0; i < DENSITIES.length; i++) {
             for (int j = 0; j < VERTEX_COUNTS.length; j++) {
-                Graph graph = generator.generate(VERTEX_COUNTS[j], DENSITIES[i]);
                 for (MSTAlgorithm algorithm : algorithms) {
-                    // Uruchom algorytm
-                    long time = performTest(algorithm, graph);
-                    // WYpisz info
                     String algName = algorithm.getClass().getSimpleName();
-                    System.out.println(String.format("Wykonano test (algorytm: %s; gęstość: %s; liczba wierzchołków: %s; czas: %s ms)",
-                            algName, DENSITIES[i], VERTEX_COUNTS[j], time));
+                    long sum = 0;
+                    for (int k = 0; k < NUMBER_OF_TESTS; k++) {
+                        Graph graph = generator.generate(VERTEX_COUNTS[j], DENSITIES[i]);
+                        // Uruchom algorytm
+                        long time = performTest(algorithm, graph);
+                        // WYpisz info
+                        System.out.println(String.format("Wykonano test %s (algorytm: %s; gęstość: %s; liczba wierzchołków: %s; czas: %s ms)",
+                                i, algName, DENSITIES[i], VERTEX_COUNTS[j], time));
+                        sum += time;
+                    }
                     // Zapisz wynik
-                    times.get(algName)[i][j] = time;
+                    times.get(algName)[i][j] = sum / NUMBER_OF_TESTS;
+                    // WYpisz info
+                    System.out.println(String.format("Wykonano test (algorytm: %s; gęstość: %s; liczba wierzchołków: %s; średni czas: %s ms)",
+                            algName, DENSITIES[i], VERTEX_COUNTS[j], sum / NUMBER_OF_TESTS));
                 }
             }
         }
