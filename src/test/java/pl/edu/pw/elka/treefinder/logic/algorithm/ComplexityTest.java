f539cb847868de8cc3cdfaab581de09a80055359
siekiera <mistrzsiekiera@gmail.com>
2015-01-16 11:17:13 +0100

    Testy złożoności

--- src/test/java/pl/edu/pw/elka/treefinder/logic/algorithm/ComplexityTest.java
+++ src/test/java/pl/edu/pw/elka/treefinder/logic/algorithm/ComplexityTest.java
@@ -1,6 +1,14 @@
 package pl.edu.pw.elka.treefinder.logic.algorithm;
 
+import org.junit.Test;
+import pl.edu.pw.elka.treefinder.logic.generator.GraphGenerator;
+import pl.edu.pw.elka.treefinder.logic.generator.GraphGeneratorImpl;
 import pl.edu.pw.elka.treefinder.model.Graph;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Test złożoności algorytmu
@@ -10,6 +18,38 @@ import pl.edu.pw.elka.treefinder.model.Graph;
  * @author Michał Toporowski
  */
 public class ComplexityTest {
+
+    private final MSTAlgorithm[] algorithms = {new PrimAlgorithm(), new KruskalAlgorithm()};
+    private static final float[] DENSITIES = {0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f};
+    private static final int[] VERTEX_COUNTS = {50, 100, 150, 200, 250, 300};
+    private final GraphGenerator generator = new GraphGeneratorImpl();
+
+
+    @Test
+    public void testComplexity() throws Exception {
+        Map<String, long[][]> times = new HashMap<>();
+        for (MSTAlgorithm algorithm : algorithms) {
+            times.put(algorithm.getClass().getSimpleName(), new long[DENSITIES.length][VERTEX_COUNTS.length]);
+        }
+        for (int i = 0; i < DENSITIES.length; i++) {
+            for (int j = 0; j < VERTEX_COUNTS.length; j++) {
+                Graph graph = generator.generate(VERTEX_COUNTS[j], DENSITIES[i]);
+                for (MSTAlgorithm algorithm : algorithms) {
+                    // Uruchom algorytm
+                    long time = performTest(algorithm, graph);
+                    // WYpisz info
+                    String algName = algorithm.getClass().getSimpleName();
+                    System.out.println(String.format("Wykonano test (algorytm: %s; gęstość: %s; liczba wierzchołków: %s; czas: %s ms)",
+                            algName, DENSITIES[i], VERTEX_COUNTS[j], time));
+                    // Zapisz wynik
+                    times.get(algName)[i][j] = time;
+                }
+            }
+        }
+        // Eksport wyników do pliku
+        exportResults(times);
+    }
+
 
     /**
      * Wykonuje podany algorytm dla danego grafu
@@ -23,5 +63,28 @@ public class ComplexityTest {
         algorithm.calculate(inputGraph);
         long end = System.currentTimeMillis();
         return end - start;
+    }
+
+    /**
+     * Eksportuje wyniki testu do pliku CSV
+     *
+     * @param times
+     * @throws IOException
+     */
+    private void exportResults(Map<String, long[][]> times) throws IOException {
+        for(String algName : times.keySet()) {
+            FileWriter writer = new FileWriter(algName + ".csv");
+            for (long[] row : times.get(algName)) {
+                String line = "";
+                for (long value : row) {
+                    line += Long.toString(value);
+                    line += ',';
+                }
+                line = line.substring(0, line.length() - 1) + '\n';
+                writer.append(line);
+            }
+            writer.flush();
+            writer.close();
+        }
     }
 }
